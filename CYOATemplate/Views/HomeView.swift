2bfb57f1e9dcb756fd9487561c3e0b9895fbc98d
Joyce Tao <jtao@lcs.on.ca>
2023-06-05 14:52:48 -0400

    Brief static interface for HomeView

---
+++ CYOATemplate/Views/HomeView.swift
@@ -0,0 +1,78 @@
+//
+//  HomeView.swift
+//  CYOATemplate
+//
+//  Created by Joyce Tao on 2023-06-05.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @Binding var show: Bool
+    
+    var body: some View {
+        ZStack {
+            
+            Color.black
+                .ignoresSafeArea(.all)
+                
+            VStack {
+
+                Spacer()
+                
+                Image("Coast")
+                    .resizable()
+                    .frame(maxHeight: 300)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    // Start button
+                    NavigationLink(destination: GamingView()) {
+                        Button(action: {
+                            print("start game")
+                        }, label: {
+                            HStack {
+                                Image(systemName: "house")
+                                Text("Start")
+                            }
+                        })
+                        .buttonStyle(CustomButton())
+                    }
+                    
+                    Spacer()
+                    
+                    // Summary button
+                    NavigationLink(destination: SummaryView()) {
+                        Button(action: {
+                            print("start game")
+                        }, label: {
+                            HStack {
+                                Image(systemName: "house")
+                                Text("Summary")
+                            }
+                        })
+                        .buttonStyle(CustomButton())
+                    }
+                    
+                    Spacer()
+                    
+                }
+                
+                Spacer()
+                
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView(show: .constant(true))
+    }
+}
---
+++ CYOATemplate/Views/SummaryView.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryView.swift
+//  CYOATemplate
+//
+//  Created by Joyce Tao on 2023-06-05.
+//
+
+import SwiftUI
+
+struct SummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SummaryView_Previews: PreviewProvider {
+    static var previews: some View {
+        SummaryView()
+    }
+}
