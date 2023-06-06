478b2e5387dbdcb80441d124a05da5057ca4a442
Joyce Tao <jtao@lcs.on.ca>
2023-06-05 21:13:56 -0400

    Use condition to navigate to different views

--- CYOATemplate/Views/HomeView.swift
+++ CYOATemplate/Views/HomeView.swift
@@ -9,58 +9,68 @@ import SwiftUI
 
 struct HomeView: View {
     
+    // MARK: Stored properties
+    @State var viewStatus = "main"
+    
     var body: some View {
-        ZStack {
-            
-            Color.black
-                .ignoresSafeArea(.all)
+        if viewStatus == "game" {
+            GamingView()
+        } else if viewStatus == "summary" {
+            SummaryView()
+        } else {
+            ZStack {
                 
-            VStack {
+                Color.black
+                    .ignoresSafeArea(.all)
+                    
+                VStack {
 
-                Spacer()
-                
-                Image("Coast")
-                    .resizable()
-                    .frame(maxHeight: 300)
-                    .padding(.horizontal)
-                
-                Spacer()
-                
-                HStack {
+                    Spacer()
+                    
+                    Image("Coast")
+                        .resizable()
+                        .frame(maxHeight: 300)
+                        .padding(.horizontal)
                     
                     Spacer()
                     
-                    // Start button
-                    Button(action: {
-                        print("start game")
-                    }, label: {
-                        HStack {
-                            Image(systemName: "house")
-                            Text("Start")
-                        }
-                    })
-                    .buttonStyle(CustomButton())
-                    
-                    Spacer()
-                    
-                    // Summary button
-                    Button(action: {
-                        print("ending summary")
-                    }, label: {
-                        HStack {
-                            Image(systemName: "house")
-                            Text("Summary")
-                        }
-                    })
-                    .buttonStyle(CustomButton())
+                    HStack {
+                        
+                        Spacer()
+                        
+                        // Start button
+                        Button(action: {
+                            viewStatus = "game"
+                        }, label: {
+                            HStack {
+                                Image(systemName: "house")
+                                Text("Start")
+                            }
+                        })
+                        .buttonStyle(CustomButton())
+                        
+                        Spacer()
+                        
+                        // Summary button
+                        Button(action: {
+                            viewStatus = "summary"
+                        }, label: {
+                            HStack {
+                                Image(systemName: "house")
+                                Text("Summary")
+                            }
+                        })
+                        .buttonStyle(CustomButton())
+                        
+                        Spacer()
+                        
+                    }
                     
                     Spacer()
                     
                 }
-                
-                Spacer()
-                
             }
+
         }
     }
 }
